9dde59008f6330cc1d0599029b807e61f7d1049e
mazai <mazai@gmail.com>
2020-03-26 12:51:54 +0800

    添加验证码

--- src/main/java/com/xinbo/cloud/common/vo/common/LoginVo.java
+++ src/main/java/com/xinbo/cloud/common/vo/common/LoginVo.java
@@ -17,12 +17,17 @@ public class LoginVo {
      * 用户名
      */
     @NotEmpty(message = "用户名不能为空")
-    private  String username;
+    private String username;
 
     /**
      * 用户密码
      */
     @NotEmpty(message = "密码不能为空")
-    private  String password;
+    private String password;
+
+    /**
+     * 验证码
+     */
+    private String captcha;
 
 }
