909f2a386e41aa0d06d48b91a0deb77c38149850
hanszhou80 <hanszhou80@gmail.com>
2020-05-01 16:04:18 +0800

    增加彩票玩法接口

---
+++ src/main/java/com/xinbo/cloud/common/vo/lottery/AgentLotteryPlayTypeSearchVo.java
@@ -0,0 +1,56 @@
+package com.xinbo.cloud.common.vo.lottery;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 汉斯
+ * @date 2020/4/28 11:15
+ * @desc 代理玩法配置
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgentLotteryPlayTypeSearchVo {
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 代理商户编号
+     */
+    private String merchantCode;
+    /**
+     * 彩种类型编号
+     */
+    private int categoryCode;
+    /**
+     * 彩种编号
+     */
+    private int lotteryTypeCode;
+    /**
+     * 彩种玩法编号
+     */
+    private int playTypeCode;
+    /**
+     * 彩种玩法名称
+     */
+    private String playTypeName;
+    /**
+     * 彩种玩法编号
+     */
+    private int playMode;
+
+    /**
+     * 排序号
+     */
+    private int sort;
+    /**
+     * 排序号
+     */
+    private boolean enable;
+
+}
